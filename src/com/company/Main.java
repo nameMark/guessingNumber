9e418ed15a917b30b1d8494bc20f85b988a23393
Marko <50919055+nameMark@users.noreply.github.com>
2020-04-12 10:06:49 +0200

    fixing bugs.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,58 +10,47 @@ public class Main {
         int range = 0;
         int yourNumber = 0;     // variable you are writing
         String level;
-
+        int secretNumber = 1 + (int) (Math.random() * 99); //random number inn range from 1 to 100
 
         System.out.println("  ***GUESSING MY NUMBER GAME***");
 
         System.out.print("     What level do you want?:");
         while (true) {
-
             level = scanner.nextLine().toLowerCase();
             if (level.equals("easy")) {
-                count = 30;
+                range = 20;
                 break;
             } else if (level.equals("medium")) {
-                count = 15;
+                range = 15;
                 break;
             } else if (level.equals("hard")) {
-                count = 3;
+                range = 8;
                 break;
             }
-            System.out.println("write easy/medium/hard");
+            System.out.print("write easy/medium/hard:");
         }
 
-            int secretNumber = 1 + (int) (Math.random() * 99); //random number inn range from 1 to 100
+        System.out.println("");
+        System.out.print("I am thinking of number  from 1 to 100" + " ... guess what it is?:");
 
-            System.out.println("");
-            System.out.print("I am thinking of number  from 1 to 100"
-                    + " ... guess what it is?:");
-            do {
-                yourNumber = scanner.nextInt();
-                count++;
-                if (level.equals("easy") && count < 30){
-                    System.out.println("You lose!");
-                    if (yourNumber > secretNumber) {
-                        System.out.println("lower!");
-                    } else if (yourNumber < secretNumber) {
-                        System.out.println("higher!");
-                    }
-                }
-
+        while (yourNumber != secretNumber) {
+            yourNumber = scanner.nextInt();
+            count++;
+            if (range == count) {
+                System.out.println("You lose! Try again.");
             }
-                while (yourNumber != secretNumber);
-
-
-
-
-                if (count > 1) {
-                    System.out.println("Congratulations. You guessed the number with "
-                            + count + " tries!");
-                } else {
-                    System.out.println("Congratulations. You guessed the number with "
-                            + count + ". try!");
+            else if (count < range){
+                if (yourNumber > secretNumber) {
+                    System.out.println("lower!");
+                } else if (yourNumber < secretNumber) {
+                    System.out.println("higher!");
                 }
-
-
+            }
+        }
+        if (count > 1) {
+            System.out.println("Congratulations. You guessed the number with " + count + " tries!");
+        } else {
+            System.out.println("Congratulations. You guessed the number with " + count + ". try!");
+        }
     }
 }
